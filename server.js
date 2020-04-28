dd062d3fa1ff6889a5a81e161f46c56115ec8901
Gautam Phuyal <gautamphuyal017@sxc.edu.np>
2020-04-28 21:27:00 +0545

    add todos and todos id

--- server.js
+++ server.js
@@ -20,24 +20,42 @@ var todos = [{
 app.get('/', function (req, res) {
 	res.send('Todo API Root ');
 });
-app.get('/todos/:id', function(req, res){
-	var todoId = parseInt(req.params.id, 10);
-	var matchedId;
-	todos.forEach(function(todo){
-		if(todoId=== todo.id){
-			matchedId = todoId;
-		}
-	});
-	if(matchedId){
-		res.json(matchedId);
-	}else{
-		res.status(404).send();
-	}
-});
-
 app.get('/todos', function(req, res){
 	res.json(todos);
 });
+app.get('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedIdTodos;
+
+	todos.forEach(function(todo){
+		if (todoId === todo.id){
+			matchedIdTodos = todo;
+		}
+	});
+	if (matchedIdTodos){
+		res.json(matchedIdTodos);
+	}else{
+		res.status(404).send();
+
+	}
+
+});
+// app.get('/todos/:id', function(req, res){
+// 	var todoId = parseInt(req.params.id, 10);
+// 	var matchedId;
+// 	todos.forEach(function(todo){
+// 		if(todoId=== todo.id){
+// 			matchedId = todoId;
+// 		}
+// 	});
+// 	if(matchedId){
+// 		res.json(matchedId);
+// 	}else{
+// 		res.status(404).send();
+// 	}
+// });
+
+
 
 app.listen(PORT, function(){
 	console.log('Express listening the port: ' +  PORT + '!!!');
